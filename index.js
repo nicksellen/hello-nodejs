43c15b89b91c01dbf8a1261a9d959d93ac307763
Nick Sellen <git@nicksellen.co.uk>
2017-08-02 12:13:56 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const app = require('express')();
+const morgan = require('morgan');
+
+const port = process.env.PORT || 3000;
+
+app.use(morgan('combined'));
+
+app.get('/', (req, res) => {
+  res.status(200).send('hello');
+});
+
+app.listen(port, () => {
+  console.log('listening on port', port);
+});
