d06d8a491233fc169aa5368e52d29391e0ae76f3
Nick Sellen <git@nicksellen.co.uk>
2018-05-23 00:41:34 +0100

    Revert message, add exit handling

--- index.js
+++ index.js
@@ -6,9 +6,13 @@ const port = process.env.PORT || 3000;
 app.use(morgan('combined'));
 
 app.get('/', (req, res) => {
-  res.status(200).send('hello again');
+  res.status(200).send('hello');
 });
 
 app.listen(port, () => {
   console.log('listening on port', port);
 });
+
+process.on('SIGINT', () => process.exit(0));
+process.on('SIGTERM', () => process.exit(0));
+
